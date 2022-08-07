69dcfbfa2b55b4a471d0650b526b39a3d5569f5f
James Vongampai <jaythomasv29@gmail.com>
2022-08-06 18:05:50 -0700

    add portoflio page and components

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,8 +22,8 @@ const Footer = ({ data }) => {
           <ul className="copyright">
             <li>
               Made by{" "}
-              <a title="PAPA" href="http://www.papareact.com/">
-                PAPA
+              <a title="James Vongampai" href="http://www.github.com/jaythomasv29/">
+                James Vongampai
               </a>
             </li>
           </ul>
